3f9228c63a6b09075a1b72fbdfc81f2161d1525e
ABee-Tech <42933600+ABee-Tech@users.noreply.github.com>
2020-04-29 15:37:12 +0545

    Added Login and Design

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+
+$('.carousel').carousel({
+    interval: 5000,
+    keyboard: true,
+    pause: "hover",
+    ride: false,
+    wrap: true
+  });
+
+
+
+function logIn() {
+  var email = document.getElementById('email').value;
+  var pass = document.getElementById('pass').value;
+  
+  if (email == "email" && pass == "pass") {
+    alert('You are successfully logged in!');
+    window.location.href='Home.htm';
+  } else {
+    $('#sthgWrong').show();
+  }
+}
