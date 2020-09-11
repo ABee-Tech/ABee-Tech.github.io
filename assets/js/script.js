729e876dddb8908e745f7fc0f3ab37eae1dc58bc
ABee-Tech <42933600+ABee-Tech@users.noreply.github.com>
2020-09-11 17:02:18 +0545

    Update 1.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,21 @@
 
 $('.carousel').carousel({
-    interval: 5000,
-    keyboard: true,
-    pause: "hover",
-    ride: false,
-    wrap: true
-  });
+  interval: 5000,
+  keyboard: true,
+  pause: "hover",
+  ride: false,
+  wrap: true
+});
 
+$(document).ready(function(){
+  var loginStateGet = localStorage.getItem("myAppsLoginState");
 
+  
+  if (loginStateGet == null){
+    localStorage.setItem("myAppsLoginState", 0);
+  }
+  
+})
 
 function logIn() {
   var email = document.getElementById('email').value;
@@ -16,7 +24,22 @@ function logIn() {
   if (email == "email" && pass == "pass") {
     alert('You are successfully logged in!');
     window.location.href='home.htm';
+    localStorage.setItem("myAppsLoginState", 1);
   } else {
     $('#sthgWrong').show();
   }
+}
+
+function logOut() {
+  
+  var loginState = localStorage.getItem("myAppsLoginState");
+  if (loginState == 0 || loginState == null){
+    alert('You are not logged in to do so!');
+    
+  } else if (loginState == 1){
+    alert('You are successfully logged out!');
+    window.location.href='index.html';
+    localStorage.setItem("myAppsLoginState", 0);
+  }
+  
 }
